c49b79f07fee5f02dc6f74d390e61e50d1c7e2d5
dado.bajramovic@gmail.com <dado.bajramovic@gmail.com@bf4bfd33-75b5-3386-34b6-6f15fd1b7053>
2012-04-25 05:03:40 +0000

    -impelementirane klase Dogadjaj.cs i DogadjajDAO.cs

---
+++ trunk/DAL/DAO/DogadjajDAO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Interfejsi;
+using DAL.Entiteti;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    partial class DAL
+    {
+        public class DogadjajDAO : IDaoCrud<Dogadjaj>
+        {
+            #region IDaoCrud<Dogadjaj> Members
+            protected MySqlCommand c;
+
+            public long create(Dogadjaj entity)
+            {
+                try
+                {
+                    c = new MySqlCommand("insert into dogadjaji values ('" + entity.ImeDrugogUcesnika + "','" + entity.ImeDrugogUcesnika + "','"+ Convert.ToString(entity.Kvota_1)+"','"+ Convert.ToString(entity.Kvota_X)+ "','"+ Convert.ToString(entity.Kvota_2) + "','" + Convert.ToString(entity.Kvota_1X) + "','" + Convert.ToString(entity.Kvota_X2) +"','"+ Convert.ToString(entity.Kvota_12) + "')", con);
+                    c.ExecuteNonQuery();
+                    return c.LastInsertedId;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public List<Dogadjaj> getAll()
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from dogadjaji", con);
+                    MySqlDataReader r = c.ExecuteReader();
+                    List<Dogadjaj> dogadjaji = new List<Dogadjaj>();
+                    while (r.Read())
+                        dogadjaji.Add(new Dogadjaj(r.GetString("imeprvogucesnika"), r.GetString("imedrugogucesnika"), r.GetDouble("kvota1"), r.GetDouble("kvota1"), r.GetDouble("kvota1"), r.GetDouble("kvota1"), r.GetDouble("kvota1"), r.GetDouble("kvota1")));
+                    return dogadjaji;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+
+            public Dogadjaj read(Dogadjaj entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Dogadjaj update(Dogadjaj entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void delete(Dogadjaj entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Dogadjaj getById(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Dogadjaj> getByExample(string name, string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ trunk/DAL/Entiteti/Dogadjaj.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Entiteti
+{
+    public class Dogadjaj
+    {
+        long id_dogadjaja;
+        String imePrvogUcesnika;
+        String imeDrugogUcesnika;
+        double kvota_1, kvota_2, kvota_X, kvota_1X, kvota_X2, kvota_12;
+        int Rezultat;
+        public Dogadjaj(String ipu, String idu, double K1, double KX, double K2, double K1X, double KX2, double K12)
+        {
+            imePrvogUcesnika = ipu;
+            imeDrugogUcesnika = idu;
+            kvota_1 = K1;
+            kvota_X = KX;
+            kvota_2 = K2;
+            kvota_1X = K1X;
+            kvota_X2 = KX2;
+            kvota_12 = K12;
+            Rezultat = -1;
+        }
+        ~Dogadjaj() { }
+        public long ID_Dogadjaja
+        {
+            get { return id_dogadjaja; }
+            set { id_dogadjaja = value; }
+        }
+        public string ImePrvogUcesnika
+        {
+            get { return imePrvogUcesnika; }
+            set { imePrvogUcesnika = value; }
+        }
+        public string ImeDrugogUcesnika { 
+            get { return imeDrugogUcesnika; }
+            set { imeDrugogUcesnika = value; }
+        }
+        public double Kvota_1
+        {
+            get { return kvota_1; }
+            set { kvota_1 = value; }
+        }
+        public double Kvota_2 { 
+
+            get { return kvota_2; }
+            set { kvota_2 = value; }
+        }
+        public double Kvota_X { 
+            get { return kvota_X; }
+            set { kvota_X = value; }
+        }
+        public double Kvota_1X { 
+            get { return kvota_1X; }
+            set { kvota_1X = value; }
+        }
+        public float Kvota_X2 { 
+            get { return Kvota_X2; }
+            set { kvota_X2 = value; }
+        }
+        public float Kvota_12 { 
+            get { return Kvota_12; }
+            set { kvota_X2 = value; }
+        }
+    }
+}
