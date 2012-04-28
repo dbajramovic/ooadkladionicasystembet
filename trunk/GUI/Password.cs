36367b99b566649f542d9235547da8337d1aa95a
dado.bajramovic@gmail.com <dado.bajramovic@gmail.com@bf4bfd33-75b5-3386-34b6-6f15fd1b7053>
2012-04-28 10:05:27 +0000

    asd

---
+++ trunk/GUI/Password.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Password : Form
+    {
+        public Password()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text == "w00t")
+            {
+                RegistracijaZaposlenika rz = new RegistracijaZaposlenika();
+                rz.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Pogrešan password!");
+        }
+    }
+}
