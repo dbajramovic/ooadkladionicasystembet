20f56dcb623f8dc6ae4df6bc1fa40d68ffde369d
dahmic2@etf.unsa.ba <dahmic2@etf.unsa.ba@bf4bfd33-75b5-3386-34b6-6f15fd1b7053>
2012-05-05 10:23:16 +0000

    Implementacija Tiket.cs i TiketDAO.cs

---
+++ trunk/DAL/DAO/TiketDAO.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Interfejsi;
+using DAL.Entiteti;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    partial class DAL
+    {
+        public class TiketDAO : IDaoCrud<Tiket>
+        {
+            #region IDaoCrud<Tiket> Members
+            protected MySqlCommand c;
+
+            public long create(Tiket entity)
+            {
+                try
+                {
+                    c = new MySqlCommand("insert into tiketi values ('" + Convert.ToString(entity.Datum) + "','" + Convert.ToString(entity.Da_Li_Je_Clan_Uplatio) + "','" + Convert.ToString(entity.Uplata) + "','" + Convert.ToString(entity.Dobitak) + "')", con);
+                    c.ExecuteNonQuery();
+                    return c.LastInsertedId;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public List<Tiket> getAll()
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from tiketi", con);
+                    MySqlDataReader r = c.ExecuteReader();
+                    List<Tiket> tiketi = new List<Tiket>();
+                    while (r.Read())
+                        tiketi.Add(new Tiket(r.GetDateTime("datum"), r.GetBoolean("clanski_broj"), r.GetDouble("uplata"), r.GetDouble("dobitak")));
+                    return tiketi;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+
+            public Tiket read(Tiket entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Tiket update(Tiket entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void delete(Tiket entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Tiket getById(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Tiket> getByExample(string name, string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+    }
+}
+
