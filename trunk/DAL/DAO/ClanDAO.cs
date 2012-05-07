ef59e8f76e119a42018f128ab7559999183e04d6
sukrija.amna@gmail.com <sukrija.amna@gmail.com@bf4bfd33-75b5-3386-34b6-6f15fd1b7053>
2012-05-07 10:22:58 +0000

    Ja sam pokusala da uradim klase Osoba, Uposlenik i Clan.. dodala sam i OsobaDAO.. imam neku gresku kad kompajliram (UnosCLana?! nesto ne valja, to nije do mene :P)Ja sam pokusala da uradim klase Osoba, Uposlenik i Clan.. dodala sam i OsobaDAO, ClanDAO, UposlenikDAO..

---
+++ trunk/DAL/DAO/ClanDAO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Interfejsi;
+using DAL.Entiteti;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    partial class DAL
+    {
+        public class ClanDAO : IDaoCrud<Clan>
+        {
+            #region IDaoCrud<Clan> Members
+            protected MySqlCommand c;
+
+            public long create(Clan entity)
+            {
+                try
+                {
+                    c = new MySqlCommand("insert into clan values ('" + entity.DajIDClana + "')", con);
+                    c.ExecuteNonQuery();
+                    return c.LastInsertedId;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public List<Clan> getAll()
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from clan", con);
+                    MySqlDataReader r = c.ExecuteReader();
+                    List<Clan> clanovi = new List<Clan>();
+                    while (r.Read())
+                        clanovi.Add(new Clan(r.GetDouble("id")));
+                    return clanovi;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+
+            public Clan read(Clan entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Clan update(Clan entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void delete(Clan entity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Clan getById(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Clan> getByExample(string name, string value)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ trunk/DAL/Entiteti/Osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Entiteti
+{
+    public class Osoba
+    {
+        String ImePrezime;
+        String JMBG;
+        public Osoba(String i, String j)
+        {
+            ImePrezime = i;
+            JMBG = j;
+        }
+
+        public String Ime
+        {
+            get { return ImePrezime; }
+            set { ImePrezime = value; }
+        }
+
+        public String Jmbg
+        {
+            get { return JMBG; }
+            set { JMBG = value; }
+        }
+    }
+}
+
